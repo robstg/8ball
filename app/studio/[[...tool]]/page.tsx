5dde658613d37fd18c9501c2ffa40c5cd50f020a
robstg <robstgeorge@gmail.com>
2026-04-07 14:42:26 +1200

    uggh 69

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,19 +1,22 @@
-/**
- * This route is responsible for the built-in authoring environment using Sanity Studio.
- * All routes under your studio path is handled by this file using Next.js' catch-all routes:
- * https://nextjs.org/docs/routing/dynamic-routes#catch-all-routes
- *
- * You can learn more about the next-sanity package here:
- * https://github.com/sanity-io/next-sanity
- */
+'use client'
 
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../sanity.config'
 
 export const dynamic = 'force-dynamic'
 
-export { metadata, viewport } from 'next-sanity/studio'
-
 export default function StudioPage() {
-  return <NextStudio config={config} />
+  return (
+    <div style={{ 
+      position: 'fixed', 
+      top: 0, 
+      left: 0, 
+      width: '100vw', 
+      height: '100vh', 
+      zIndex: 99999,
+      backgroundColor: 'white' 
+    }}>
+      <NextStudio config={config} />
+    </div>
+  )
 }
