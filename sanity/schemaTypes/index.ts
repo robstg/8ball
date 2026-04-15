a4058c14637c531eb7cd013f0b0aec42fd4627f4
robstg <robstgeorge@gmail.com>
2026-04-15 15:39:51 +1200

    layout rules

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,8 +1,10 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import post from './post'
 import page from './page'
-import category from './category' // 1. Import your new category file
+import category from './category' 
+import rule from './rule' // 1. Import your new rule schema
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, page, category], // 2. Add category to this list
+  // 2. Add 'rule' to this list
+  types: [post, page, category, rule], 
 }
