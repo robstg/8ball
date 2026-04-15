1a23bb40c6decc5a8b442fb7a6bec036cbc71dd6
robstg <robstgeorge@gmail.com>
2026-04-15 13:56:40 +1200

    link fix

--- components/header.tsx
+++ components/header.tsx
@@ -60,7 +60,7 @@ export function Header() {
             <Link href="/" className={linkStyle('/')}>Home</Link>
             <Link href="/pool" className={linkStyle('/pool')}>Pool</Link>
             <Link href="/snooker" className={linkStyle('/snooker')}>Snooker</Link>
-            <Link href="/about" className={linkStyle('/about')}>About</Link>
+            <Link href="/about-us" className={linkStyle('/about')}>About</Link>
           </nav>
         </div>
 
