65e2d76be13a27a066e3226e85eb75618e185788
robstg <robstgeorge@gmail.com>
2026-05-31 12:44:32 +1200

    news link

--- components/header.tsx
+++ components/header.tsx
@@ -61,6 +61,7 @@ export function Header() {
             <Link href="/pool" className={linkStyle('/pool')}>Pool</Link>
             <Link href="/snooker" className={linkStyle('/snooker')}>Snooker</Link>
             <Link href="/9-ball" className={linkStyle('/9-ball')}>9-Ball</Link>
+                <Link href="/news" className={linkStyle('/news')}>News</Link>
             <Link href="/about-us" className={linkStyle('/about')}>About</Link>
           </nav>
         </div>
